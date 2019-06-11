b265e3abbb55e74ce16a402feb8d32d2f2f6ee9f
Yuji Ogihara <yuji.ogihara.85@gmail.com>
2019-06-11 16:12:58 +0900

    First version

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 window.onload = function() {
   
-  var val = 6 ;
+  val = getParam('value')
+  if (val != null) {
+    setValue(val)
+  }
+}
   
-  var off_y = val * 80;
-  var bottom_y = off_y + 80;
-  var margin_top_y = 176 - off_y;
+function setValue(val) {  
+  var digit = 9 ;
+  var prev = val % 10 ;
   
-  cssRules =  document.styleSheets.item( 0 ).cssRules;
-  style = cssRules.item (0).style;
-  style.setProperty( 'clip' , 'rect(' + off_y + 'px,80px,' + bottom_y + 'px, 0px)' );
-  style.setProperty( 'margin-top' , margin_top_y + 'px'  );
+  for (  var i = 0;  i <7;  i++  ) {
+    val = val / 10;
+    digit = parseInt(val % 10);
+    var off_y = digit * 50 + 5 * prev;
+      
+    if (digit != 9) {
+      prev = 0;
+    } 
+    var bottom_y = off_y + 50;
+    var margin_top_y = 121 - off_y;
+    
+    cssRules =  document.styleSheets.item( 0 ).cssRules;
+    style = cssRules.item (6-i).style;
+    style.setProperty( 'clip' , 'rect(' + off_y + 'px,80px,' + bottom_y + 'px, 0px)' );
+    style.setProperty( 'margin-top' , margin_top_y + 'px'  );
+  }
+}
+
+function getParam(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
